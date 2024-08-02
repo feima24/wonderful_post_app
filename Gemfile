c0efebaeb1fc899f7fcdb2a46481be3f566509eb
test.txtにテキトーな文章を追加 <>
2024-08-03 05:41:47 +0900

    ページネーションの設定

--- Gemfile
+++ Gemfile
@@ -37,6 +37,9 @@ gem 'rails-ujs'
 
 gem 'devise'
 
+#ページネーション
+gem 'kaminari'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,6 +1,9 @@
 class MypageController < ApplicationController
+  # ユーザーがログインしているか確認するためのコールバック
+  before_action :authenticate_user!
+
   # GET /mypage
   def show
-    @articles = current_user.articles
+    @articles = current_user.articles.page(params[:page])
   end
 end
